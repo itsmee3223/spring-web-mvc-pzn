c9372ed56267870e79a7f0f2329b9af777a0c908
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-22 13:21:58 +0700

    init spring mvc

---
+++ src/test/java/ramanda/ajisaka/asyraf/spring/mvc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ramanda.ajisaka.asyraf.spring.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
