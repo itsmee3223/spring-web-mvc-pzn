afc9bf80ec70ffdf3c3cfe0374a614e1783a4925
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-22 15:19:56 +0700

    request param

---
+++ src/main/java/ramanda/ajisaka/asyraf/spring/mvc/controller/DateController.java
@@ -0,0 +1,20 @@
+package ramanda.ajisaka.asyraf.spring.mvc.controller;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class DateController {
+    private final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+
+    @GetMapping(path = "/date")
+    public void getDate(@RequestParam(name = "date") Date date, HttpServletResponse response) throws IOException {
+        response.getWriter().println("Date: " + simpleDateFormat.format(date));
+    }
+}
--- src/main/java/ramanda/ajisaka/asyraf/spring/mvc/controller/HelloController.java
+++ src/main/java/ramanda/ajisaka/asyraf/spring/mvc/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import ramanda.ajisaka.asyraf.spring.mvc.service.HelloService;
 
 import java.io.IOException;
@@ -19,8 +20,9 @@ public class HelloController {
 
 //    @RequestMapping(path = "/hello", method = RequestMethod.GET)
     @GetMapping(path = "/hello")
-    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
+//    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void helloWorld(@RequestParam(name = "name", required = false) String name, HttpServletResponse response) throws IOException {
+//        String name = request.getParameter("name");
         String bodyResponse = helloService.hello(name);
         response.getWriter().println(bodyResponse);
     }
